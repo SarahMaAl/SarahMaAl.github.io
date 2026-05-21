eea8ca4acf7f240114a0f21c8afad6ea08a74e5d
Sarah MA <143753714+SarahMaAl@users.noreply.github.com>
2026-05-21 15:57:44 +0200

    Add Astro configuration for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,1 +1,10 @@
+import { defineConfig } from "astro/config";
 
+const USERNAME = "SarahMaAl";
+const REPOSITORY = "portfolio";
+
+export default defineConfig({
+  site: `https://${USERNAME}.github.io`,
+  base: REPOSITORY ? `/${REPOSITORY}` : "/",
+  trailingSlash: "always"
+});
